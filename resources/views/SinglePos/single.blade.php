e89d4a48d013ac36473c839884a091628a44c30d
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-02-12 00:55:32 +0700

    Update single.blade.php
    showing pos name and description

--- resources/views/SinglePos/single.blade.php
+++ resources/views/SinglePos/single.blade.php
@@ -137,7 +137,8 @@
                         @csrf
                     {{-- Header --}}
                     <div class="card-header text-center" style="background-color:#ffffff;">
-                        <h2 style="color:rgba(0, 0, 0, 0.704); font-weight: bold">Pos Single</h2>
+                        <h2 style="color:rgba(0, 0, 0, 0.704); font-weight: bold">{{ $penposData->nama }}</h2>
+                        <p>{{ $penposData->deskripsi }}</p>
                     </div>
                     {{-- End Header --}}
 
