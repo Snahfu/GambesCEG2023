ebe8da42f7599261aecf1ee3f308857768118129
alexanderkenrick <alexanderkenrick3@gmail.com>
2023-02-12 14:05:16 +0700

    Tes Sidebar

--- resources/views/SinglePos/single.blade.php
+++ resources/views/SinglePos/single.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Single Pos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    {{-- font --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap" rel="stylesheet">
-    {{-- end font --}}
+@section('content')
     <style>
         body {
             background-color: #f8fefae4
@@ -88,118 +77,76 @@
             border-radius: 50%;
         }
     </style>
-</head>
 
-<body style="background: url('{{ asset('assets/background/background.png') }}') center / cover no-repeat fixed">
-    {{-- Navbar --}}
-    <div id="app" class="d-flex justify-content-center" style="z-index: 2">
 
-        <nav class="navbar navbar-expand-md navbar-light transparent" style="width: 90%;border-radius: 20px;">
-            <div class="container" style="border-radius: 20px;">
-                <a class="navbar-brand" href="{{ route('penpos.HomePenpos') }}">
-                    <img src="{{ asset('assets') }}/logo/Kelapa_navbar.png" alt="Kelapa" style="max-height: 40px">
-                    <img src="{{ asset('assets') }}/logo/Logo CEG.png" alt="Logo CEG" style="max-height: 40px">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="{{ route('penpos.historybattle') }}"
-                                style="color:aquamarine; font-weight: bold">Histori</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('logout') }}"
-                                style="color:red; font-weight: bold;"
-                                onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">Log
-                                Out</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                    </ul>
+<div class="row">
+        <div class="card p-0">
+            <form action="{{ route('penpos.insertHasil') }}" method="POST">
+                @csrf
+            {{-- Header --}}
+            <div class="card-header text-center" style="background-color:#ffffff;">
+                <h2 style="color:rgba(0, 0, 0, 0.704); font-weight: bold">{{ $penposData->nama }}</h2>
+                <p>{{ $penposData->deskripsi }}</p>
+            </div>
+            {{-- End Header --}}
+
+            {{-- Body --}}
+            
+            <div class="row d-flex justify-content-center mb-4 pt-4"
+                style="text-align: center; font-weight: bold;">
+                <div class="col-2" style="font-size: 18px;">
+                    Nama Tim :
+                </div>
+                <div class="col-2">
+                    <select name="team[]" id="team1" class="form-select"
+                        aria-label="Default select example" style="text-align: center;">
+                        <option selected hidden>--Pilih Pemain 1--</option>
+                        {{-- semua team yang belum main di pos ini --}}
+                        @foreach ($teams as $team)
+                            <option value="{{ $team->id }}">{{ $team->nama }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-2">
+                    <select name="hasil[]" class="form-select" aria-label="Default select example"
+                        style="text-align: center;">
+                        <option selected hidden>--Input Hasil--</option>
+                        <option value="menang">Menang</option>
+                        <option value="kalah">Kalah</option>
+                    </select>
+                </div>
+                <div class="col-2">
+                    <select name="koin[]" class="form-select" aria-label="Default select example"
+                        style="text-align: center;">
+                        <option selected hidden>--Input Koin--</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="75">75</option>
+                        <option value="100">100</option>
+                    </select>
                 </div>
             </div>
-        </nav>
-    </div>
-    {{-- End Navbar --}}
-
-
-    {{-- Pos --}}
-    <div class="container" style="margin-top: 50px">
-        <div class="row">
-                <div class="card p-0">
-                    <form action="{{ route('penpos.insertHasil') }}" method="POST">
-                        @csrf
-                    {{-- Header --}}
-                    <div class="card-header text-center" style="background-color:#ffffff;">
-                        <h2 style="color:rgba(0, 0, 0, 0.704); font-weight: bold">{{ $penposData->nama }}</h2>
-                        <p>{{ $penposData->deskripsi }}</p>
-                    </div>
-                    {{-- End Header --}}
-
-                    {{-- Body --}}
-                    
-                    <div class="row d-flex justify-content-center mb-4 pt-4"
-                        style="text-align: center; font-weight: bold;">
-                        <div class="col-2" style="font-size: 18px;">
-                            Nama Tim :
-                        </div>
-                        <div class="col-2">
-                            <select name="team[]" id="team1" class="form-select"
-                                aria-label="Default select example" style="text-align: center;">
-                                <option selected hidden>--Pilih Pemain 1--</option>
-                                {{-- semua team yang belum main di pos ini --}}
-                                @foreach ($teams as $team)
-                                    <option value="{{ $team->id }}">{{ $team->nama }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-2">
-                            <select name="hasil[]" class="form-select" aria-label="Default select example"
-                                style="text-align: center;">
-                                <option selected hidden>--Input Hasil--</option>
-                                <option value="menang">Menang</option>
-                                <option value="kalah">Kalah</option>
-                            </select>
-                        </div>
-                        <div class="col-2">
-                            <select name="koin[]" class="form-select" aria-label="Default select example"
-                                style="text-align: center;">
-                                <option selected hidden>--Input Koin--</option>
-                                <option value="25">25</option>
-                                <option value="50">50</option>
-                                <option value="75">75</option>
-                                <option value="100">100</option>
-                            </select>
-                        </div>
-                    </div>
-                    {{-- End Body --}}
-                    <!--Button Submit-->
-                    <div class="d-flex justify-content-center mb-4">
-                        <button type="submit" class="btn btn-outline-primary">Submit</button>
-                    </div>
-                    <!--End Button Submit-->
-                    </form>
-                {{-- Footer --}}
-                <div class="card-footer pt-3 pb-3 text-center bg-opacity-75" id="posFooter"
-                style="{{ $penposData->status == 'KOSONG' ? 'background-color: #008917; ' : 'background-color:#e2626b;' }} text-align: center; font-weight:bold; color:white;">
-                Status Pos : <span
-                    id="statusPos">{{ $penposData->status == 'KOSONG' ? 'KOSONG' : 'PENUH' }}</span>
-                <label class="switch" style="height = 10px">
-                    <input type="checkbox" id="statusCheckbox"
-                        {{ $penposData->status == 'KOSONG' ? 'value=KOSONG' : 'Checked value=PENUH' }}>
-                    <span class="slider round"></span>
-                </label>
-                </div>
-                {{-- End Footer --}}
+            {{-- End Body --}}
+            <!--Button Submit-->
+            <div class="d-flex justify-content-center mb-4">
+                <button type="submit" class="btn btn-outline-primary">Submit</button>
             </div>
+            <!--End Button Submit-->
+            </form>
+        {{-- Footer --}}
+        <div class="card-footer pt-3 pb-3 text-center bg-opacity-75" id="posFooter"
+        style="{{ $penposData->status == 'KOSONG' ? 'background-color: #008917; ' : 'background-color:#e2626b;' }} text-align: center; font-weight:bold; color:white;">
+        Status Pos : <span
+            id="statusPos">{{ $penposData->status == 'KOSONG' ? 'KOSONG' : 'PENUH' }}</span>
+        <label class="switch" style="height = 10px">
+            <input type="checkbox" id="statusCheckbox"
+                {{ $penposData->status == 'KOSONG' ? 'value=KOSONG' : 'Checked value=PENUH' }}>
+            <span class="slider round"></span>
+        </label>
         </div>
+        {{-- End Footer --}}
     </div>
+</div>
     {{-- End Pos --}}
 {{-- Scripts --}}
 <script type="text/javascript">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,26 +23,227 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <!----Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+          @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+body{
+    min-height: 100vh;
+    transition: all 0.3s ease;
+}
+ .sidebar{
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 250px;
+    padding: 10px 14px;
+    background: #242526;
+    transition: all 0.3s ease;
+    z-index: 100;  
+}
+.sidebar.close{
+    width: 88px;
+}
+
+.menu{
+    padding-left: 0 !important;
+}
+header{
+    height: 100px;
+    margin-top: 5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+header img{
+    width: 80px;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.3s ease;
+}
+.sidebar.close img{
+    width: 30px;
+}
+.sidebar .toggle{
+    position: absolute;
+    top: 5%;
+    right: 30px;
+    height: 25px;
+    width: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 26px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    color: #fff;
+}
+.menu li{
+    height: 50px;
+    list-style: none;
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+}
+.menu li i{
+    min-width: 60px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 18px;
+    transition: all 0.3s ease;
+    color: #ccc;
+}
+li span{
+    font-size: 16px;
+    font-weight: 500;
+    white-space: nowrap;
+    opacity: 1;
+    transition: .3s;
+    color: #ccc;
+}
+.sidebar.close .text{
+    opacity: 0;
+}
+.sidebar .menu{
+    margin-top: 20px;
+}
+li.search-box{
+    border-radius: 6px;
+    background-color: #3a3b3c;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+li.search-box input{
+    height: 100%;
+    width: 100%;
+    outline: none;
+    border: none;
+    background-color: #3a3b3c;
+    color: #ccc;
+    border-radius: 6px;
+    font-size: 17px;
+    font-weight: 500;
+    transition: all 0.3s ease;
+}
+li a{
+    list-style: none;
+    height: 100%;
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    border-radius: 6px;
+    text-decoration: none;
+    transition: all 0.3s ease;
+}
+li a:hover{
+    background-color: #fff;
+}
+li a:hover .icon,
+li a:hover .text{
+    color: #242526;
+}
+    </style>
 </head>
 
 <body style="background: url('{{ asset('assets') }}/background/background.png') center / cover no-repeat fixed">
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light transparent">
-            <div class="container flex-lg-row flex-nowrap align-items-center">
-                <div class="navbar-brand w-100" id="navbarSupportedContent">
-                    <img style="width: 50px; height:50px;" src="{{ asset('assets/logo/Kelapa_navbar.png ') }}"
-                                srcset="{{ asset('assets/logo/Kelapa_navbar.png ') }}" alt="" />
-                    <a href="{{ route('login') }}" style="text-decoration: none; color:aquamarine; font-weight: bold">
-                        CEG UBAYA 2023
-                    </a>
-                </div>
-            </div>
+
+    <div class="wrapper">
+        <nav class="sidebar close">
+            <header>
+                <img src="{{ asset('assets/logo/Logo CEG.png') }}" class="img-fluid logo">
+                <i class="fa-solid fa-bars toggle"></i>
+            </header>
+            
+                <ul class="menu">
+    
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class="fa-solid fa-house icon"></i>
+                            <span class="text">Home</span>
+                        </a>
+                    </li>
+    
+                    {{-- <li class="nav-link">
+                        <a href="#">
+                            <i class="fa-sharp fa-solid fa-chart-simple icon"></i>
+                            <span class="text">Revenue</span>
+                        </a>
+                    </li>
+     --}}
+                    {{-- <li class="nav-link">
+                        <a href="#">
+                            <i class="fa-solid fa-bell icon"></i>
+                            <span class="text">Notifications</span>
+                        </a>
+                    </li> --}}
+    
+                    {{-- <li class="nav-link">
+                        <a href="#">
+                            <i class="fa-solid fa-wallet icon"></i>
+                            <span class="text">Wallets</span>
+                        </a>
+                    </li> --}}
+    
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class="fa-solid fa-right-from-bracket icon"></i>
+                            <span class="text">Login</span>
+                        </a>
+                    </li>
+                </ul>
         </nav>
+
+        <div id="content" style="margin-left:88px;">
+            <div class="container-fluid py-5">
+                @yield('content')  
+            </div>           
+        </div>
+    </div>    
+  
+    {{-- <!--Main layout-->
+    <main style="margin-top: 58px;">
+        <div class="container pt-4">
+            @yield('content')
+        </div>
+    </main> --}}
+        {{-- <!--Main layout-->
 
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+    </div> --}}
+
+    {{-- <script>
+        $(document).ready(function(){
+            $("#sidebarCollapse").on('click',function(){
+                $("#sidebar").toggleClass('active');
+            });
+        });
+    </script> --}}
+
+    <script>
+        const body = document.querySelector('body'),
+            sidebar = body.querySelector('nav'),
+            toggle = body.querySelector(".toggle");
+
+        toggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+        })
+    </script>
 </body>
 
 </html>
