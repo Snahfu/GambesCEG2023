00fe3c8057df2ac4c13405ba0def9385972dcb17
FransiscusSuhargo <s160422070@student.ubaya.ac.id>
2023-02-12 19:41:56 +0700

    Buy and Sell

--- resources/views/pemain/buy.blade.php
+++ resources/views/pemain/buy.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.app')
 @section('content')
+<style>
+ *{
+    font-family: 'Poppins', 'sans-serif';
+}
+.btn-primary{
+    background-color: white;
+    color: blue;
+}
+.btn-primary:hover{
+    background-color: green;
+}
+</style>
+<div class="container px-4 py-2">
 <div class="card">
     <div class="card-header text-center">
-        <h3>Buy</h3>
+        <h3 style ="font">Buy Card</h3>
     </div>
     <div class="card-body">
         {{-- START Baris Pertama --}}
@@ -11,35 +24,40 @@
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h4 class="card-title">Kartu 1</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 1</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 2</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 1</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 3</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 1</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 4</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 1</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 5</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
         </div>
@@ -50,36 +68,41 @@
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 2</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 6</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 2</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 7</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 2</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 8</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 2</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 9</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 2</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 10</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
         </div>
@@ -90,40 +113,46 @@
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 3</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 11</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 3</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 12</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 3</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 13</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 3</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 14</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
             <div class="card m-2 col-2 p-0">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Kartu 3</h4>
-                    <button class="btn btn-primary">BUY</button>
+                    <h4 class="card-title">Kartu 15</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Buy</button>
                 </div>
             </div>
         </div>
         {{-- END Baris Ketiga --}}
     </div>
+</div>
 </div>
 @endsection
--- resources/views/pemain/sell.blade.php
+++ resources/views/pemain/sell.blade.php
@@ -1,4 +1,33 @@
 @extends('layouts.app')
 @section('content')
-Ini Jual
+<style>
+*{
+    font-family: 'Poppins', 'sans-serif';
+}
+.btn-primary{
+    background-color: white;
+    color: blue;
+}
+.btn-primary:hover{
+    background-color: red;
+}
+</style>
+<div class="container px-4 py-2">
+<div class="card">
+    <div class="card-header text-center">
+        <h3 style ="font">Sell Card</h3>
+    </div>
+    <div class="card-body">
+        <div class="row justify-content-left">
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 1</h4>
+                    <h5 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-coins" id="icon-coin"></i><span>        100</span></h5>
+                    <button class="btn btn-primary">Sell</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
