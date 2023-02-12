422f2c95d22ea5994fa52ead1e4d261c37b11e0c
ZahwaNovario <novario120@gmail.com>
2023-02-12 18:29:40 +0700

    Add Buy Page

--- resources/views/pemain/buy.blade.php
+++ resources/views/pemain/buy.blade.php
@@ -1,4 +1,129 @@
 @extends('layouts.app')
 @section('content')
-Ini Beli
+<div class="card">
+    <div class="card-header text-center">
+        <h3>Buy</h3>
+    </div>
+    <div class="card-body">
+        {{-- START Baris Pertama --}}
+        <div class="row justify-content-center">
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 1</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 1</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 1</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 1</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 1</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+        </div>
+        {{-- END Baris Pertama --}}
+
+        {{-- START Baris Kedua --}}
+        <div class="row justify-content-center">
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 2</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 2</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 2</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 2</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 2</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+        </div>
+        {{-- END Baris Kedua --}}
+
+        {{-- START Baris Ketiga --}}
+        <div class="row justify-content-center">
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 3</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 3</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 3</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 3</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+            <div class="card m-2 col-2 p-0">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/White_domesticated_duck%2C_stretching.jpg/800px-White_domesticated_duck%2C_stretching.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h4 class="card-title">Kartu 3</h4>
+                    <button class="btn btn-primary">BUY</button>
+                </div>
+            </div>
+        </div>
+        {{-- END Baris Ketiga --}}
+    </div>
+</div>
 @endsection
