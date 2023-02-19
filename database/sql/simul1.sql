211d4774ca8d4cfc5922937804c11113e46b2ba0
Snahfu <hanxin131@gmail.com>
2023-02-19 09:37:30 +0700

    update database sql

--- database/sql/simul1.sql
+++ database/sql/simul1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2023 at 03:36 AM
+-- Generation Time: Feb 19, 2023 at 03:37 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
