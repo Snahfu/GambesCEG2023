545bc7c42f2afa22f6553453d49ca16a32c59845
alexanderkenrick <alexanderkenrick3@gmail.com>
2023-04-01 10:50:58 +0700

    Update UI

--- resources/views/pemain/history.blade.php
+++ resources/views/pemain/history.blade.php
@@ -1,45 +1,57 @@
 @extends('pemain.app')
 
 @section('content')
+
+    <style>
+        .card{
+            box-shadow: 5px 10px 20px rgba(0, 0, 0, 0.2);
+            border: 0px solid black;
+            border-radius: 20px;
+        }
+    </style>
     <div class="container px-sm-4 py-2">
         <div class="row">
             <div class="card p-0">
 
                 <!--Header-->
-                <div class="card-header pt-3" style="background-color: white;text-align: center;">
+                {{-- <div class="card-header pt-3" style="background-color: white;text-align: center;">
                     <h1 style="color:rgba(0, 0, 0, 0.704); font-weight: bold">Histori</h1>
-                </div>
+                </div> --}}
                 <!--End Header-->
 
                 <!--Card Body-->
                 <div class="card-body mt-2">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Nama Tim</th>
-                                <th scope="col">Nama Pos</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Jam Ditambah</th>
-                                <th scope="col">Koin Didapat</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if ($penposteams->count())
-                                @foreach ($penposteams as $eachPenpos)
-                                    <tr>
-                                        <td>{{ $namatim }}</td>
-                                        <td>{{ $namaPenpos->find($eachPenpos->teams_id)->nama }}</td>
-                                        <td>{{ $eachPenpos->hasil }}</td>
-                                        <td>{{ $eachPenpos->jam->format('H:i') }}</td>
-                                        <td>{{ $eachPenpos->koin }}</td>
-                                    </tr>
-                                @endforeach
-                            @else
-                                <p class="text-center fs-4">No History Found</p>
-                            @endif
-
-                        </tbody>
-                    </table>
+                    <h1 style="color:rgba(0, 0, 0, 1); font-weight: bold; text-align:center">History</h1>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th scope="col">Nama Tim</th>
+                                    <th scope="col">Nama Pos</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Jam Ditambah</th>
+                                    <th scope="col">Koin Didapat</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if ($penposteams->count())
+                                    @foreach ($penposteams as $eachPenpos)
+                                        <tr>
+                                            <td>{{ $namatim }}</td>
+                                            <td>{{ $namaPenpos->find($eachPenpos->teams_id)->nama }}</td>
+                                            <td>{{ $eachPenpos->hasil }}</td>
+                                            <td>{{ $eachPenpos->jam->format('H:i') }}</td>
+                                            <td>{{ $eachPenpos->koin }}</td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <p class="text-center fs-4">No History Found</p>
+                                @endif
+    
+                            </tbody>
+                        </table>
+                    </div>
+                    
                     <div class="d-flex justify-content-end">
                         {{ $penposteams->links('pagination::bootstrap-4') }}
                     </div>
--- resources/views/pemain/sell.blade.php
+++ resources/views/pemain/sell.blade.php
@@ -13,6 +13,11 @@
         .btn-primary:hover {
             background-color: green;
         }
+        #cardSell{
+            box-shadow: 5px 10px 20px rgba(0, 0, 0, 0.2);
+            border: 0px solid black;
+            border-radius: 20px; 
+        }
 
         /* Responsive Grid Start */
         .cardSell {
@@ -92,10 +97,11 @@
     </style>
     <div class="container px-sm-4 py-2">
         <div class="card" id="cardSell">
-            <div class="card-header text-center">
-                <h3 style="font">Sell Card</h3>
-            </div>
+            {{-- <div class="card-header text-center">
+                <h1 style="color:rgba(0, 0, 0, 1); font-weight: bold; text-align:center">Buy</h1>
+            </div> --}}
             <div class="card-body">
+                <h1 style="color:rgba(0, 0, 0, 1); font-weight: bold; text-align:center">Sell Card</h1>
                 <div class="inline-spacing">
                     <div class="card-container">
                         @foreach ($inventoryKartus as $kartu)
